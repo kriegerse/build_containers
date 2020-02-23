9e6c0efc345b27ba81c2f8d65d703eb969bdd9ca
kriegerse <s_krieger@web.de>
2020-02-23 22:46:48 +0100

    change to tags_helper_script

---
+++ drone_tags_helper.sh
@@ -0,0 +1,8 @@
+#!/bin/ash
+
+if ${DRONE_BRANCH} -eq "master"; then
+  echo -n "latest,build-${DRONE_BUILD_NUMBER}" > .tags
+  echo -n "builddate-$(date +%Y-%m-%d)" >> .tags
+else
+  echo -n "testing,build-${DRONE_BUILD_NUMBER}" > .tags
+fi
