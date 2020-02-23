318a944c7939b31ba617a0a5df6fea4241c02a63
kriegerse <s_krieger@web.de>
2020-02-23 22:49:35 +0100

    fix ash fi

--- drone_tags_helper.sh
+++ drone_tags_helper.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-if ${DRONE_BRANCH} -eq "master"; then
+if [[ "${DRONE_BRANCH}" -eq "master" ]]; then
   echo -n "latest,build-${DRONE_BUILD_NUMBER}" > .tags
   echo -n "builddate-$(date +%Y-%m-%d)" >> .tags
 else
