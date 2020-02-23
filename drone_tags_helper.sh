20b6bf7f8eafb8e30f83dab1294e0a4f57c9efce
kriegerse <s_krieger@web.de>
2020-02-23 23:04:01 +0100

    add PR to tags_helper

--- drone_tags_helper.sh
+++ drone_tags_helper.sh
@@ -2,8 +2,11 @@
 
 if [[ "${DRONE_BRANCH}" -eq "master" ]]; then
   echo -n "latest,build-${DRONE_BUILD_NUMBER}" > .tags
-  echo -n "builddate-$(date +%Y-%m-%d)" >> .tags
+  echo -n ",builddate-$(date +%Y-%m-%d)" >> .tags
 else
-  echo -n "testing,build-${DRONE_BUILD_NUMBER}" > .tags
-  echo -n "branch-${DRONE_BRANCH}" >> .tags
+  echo -n "build-${DRONE_BUILD_NUMBER}" > .tags
+  echo -n ",branch-${DRONE_BRANCH}" >> .tags
+  if [[ "${DRONE_PULL_REQUEST}" -ne "" ]]; then
+    echo -n ",PR-${DRONE_PULL_REQUEST}" >> .tags
+  fi
 fi
