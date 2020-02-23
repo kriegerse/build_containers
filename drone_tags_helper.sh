183470403fdc2cb235d53c7c363a855994051337
kriegerse <s_krieger@web.de>
2020-02-23 23:10:06 +0100

    add generated tags output

--- drone_tags_helper.sh
+++ drone_tags_helper.sh
@@ -1,12 +1,12 @@
-#!/bin/ash
+#!/usr/bin/env bash
 
-if [[ "${DRONE_BRANCH}" -eq "master" ]]; then
+if [[ "${DRONE_BRANCH}" == "master" ]]; then
   echo -n "latest,build-${DRONE_BUILD_NUMBER}" > .tags
   echo -n ",builddate-$(date +%Y-%m-%d)" >> .tags
 else
   echo -n "build-${DRONE_BUILD_NUMBER}" > .tags
   echo -n ",branch-${DRONE_BRANCH}" >> .tags
-  if [[ "${DRONE_PULL_REQUEST}" -ne "" ]]; then
+  if [[ "${DRONE_PULL_REQUEST}" != "" ]]; then
     echo -n ",PR-${DRONE_PULL_REQUEST}" >> .tags
   fi
 fi
