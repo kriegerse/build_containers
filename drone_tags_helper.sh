eae2a07d4aec5ee349953afe04d44ba29d0c46b6
kriegerse <s_krieger@web.de>
2020-02-23 23:36:28 +0100

    change when condition

--- drone_tags_helper.sh
+++ drone_tags_helper.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ "${DRONE_BRANCH}" == "master" ]]; then
-  echo -n "latest,build-${DRONE_BUILD_NUMBER}" > .tags
-  echo -n ",builddate-$(date +%Y-%m-%d)" >> .tags
-else
-  echo -n "build-${DRONE_BUILD_NUMBER}" > .tags
-  echo -n ",branch-${DRONE_BRANCH}" >> .tags
-  if [[ "${DRONE_PULL_REQUEST}" != "" ]]; then
-    echo -n ",PR-${DRONE_PULL_REQUEST}" >> .tags
-  fi
-fi
+echo -n "build-${DRONE_BUILD_NUMBER}" > .tags
 
-cat ./.tags
+if [[ "${DRONE_BRANCH}" == "master" ]]; then
+  echo -n ",latest" > .tags
+  echo -n ",builddate-$(date +%Y-%m-%d)" >> .tags
+elif [[ ${DRONE_PULL_REQUEST} != "" ]]; then
+  echo -n ",PR-${DRONE_PULL_REQUEST}" >> .tags
+else
+  echo -n ",branch-${DRONE_BRANCH}" >> .tags
+fi
