6e9f2f81030c82ba6a27e265027c85c172b031c1
kriegerse <s_krieger@web.de>
2020-02-23 23:53:26 +0100

    test event

--- drone_tags_helper.sh
+++ drone_tags_helper.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env ash
 
-echo -n "build-${DRONE_BUILD_NUMBER}" > .tags
 
 if [[ "${DRONE_BRANCH}" == "master" ]]; then
-  echo -n ",latest" > .tags
+  echo -n "latest" > .tags
   echo -n ",builddate-$(date +%Y-%m-%d)" >> .tags
 elif [[ ${DRONE_PULL_REQUEST} != "" ]]; then
-  echo -n ",PR-${DRONE_PULL_REQUEST}" >> .tags
+  echo -n "pr-${DRONE_PULL_REQUEST}" >> .tags
 else
-  echo -n ",branch-${DRONE_BRANCH}" >> .tags
+  echo -n "branch-${DRONE_BRANCH}" >> .tags
 fi
