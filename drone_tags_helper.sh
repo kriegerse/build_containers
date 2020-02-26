b47c7d3af1b68ee074412d72666f8e98f948afd8
kriegerse <s_krieger@web.de>
2020-02-26 21:00:38 +0100

    remove tags creation for branch operstion

--- drone_tags_helper.sh
+++ drone_tags_helper.sh
@@ -5,7 +5,7 @@ if [[ "${DRONE_BRANCH}" == "master" ]]; then
   echo -n "latest" > .tags
   echo -n ",builddate-$(date +%Y-%m-%d)" >> .tags
 elif [[ ${DRONE_PULL_REQUEST} != "" ]]; then
-  echo -n "pr-${DRONE_PULL_REQUEST}" >> .tags
-else
-  echo -n "branch-${DRONE_BRANCH}" >> .tags
+  echo -n "pr-${DRONE_PULL_REQUEST}-build-${DRONE_BUILD_NUMBER}" >> .tags
+#else
+#  echo -n "branch-${DRONE_BRANCH}" >> .tags
 fi
