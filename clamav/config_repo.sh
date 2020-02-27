4b16d9ce6ff2f9d203d9806b686fc952bccf9938
kriegerse <s_krieger@web.de>
2020-02-27 23:41:23 +0100

    add debug for repos

--- clamav/config_repo.sh
+++ clamav/config_repo.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 
-set -x
-
-cat /etc/resolv.conf
-
-nslookup download.opensuse.org
-nslookup google.de
-
-nslookup download.opensuse.org 1.1.1.1
-nslookup google.de 1.1.1.1
-
-
-
 # get OS release info
 source /etc/os-release
 
 # add security repository and import keys
-zypper -vvv ar http://download.opensuse.org/repositories/security/openSUSE_Leap_${VERSION_ID}/security.repo
-zypper -vvv --gpg-auto-import-keys refresh
+zypper -vvv --non-interactive ar http://download.opensuse.org/repositories/security/openSUSE_Leap_${VERSION_ID}/security.repo
+zypper -vvv --gpg-auto-import-keys --non-interactive refresh
