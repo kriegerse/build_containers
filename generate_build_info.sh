965af49ad9b980832cda8f4cc55603d152aa8c5a
kriegerse <s_krieger@web.de>
2020-02-29 22:44:21 +0100

    testing PR

--- generate_build_info.sh
+++ generate_build_info.sh
@@ -1,7 +1,37 @@
 #!/usr/bin/env bash
 
 
-echo "export TEST1=TESTVAL1"
+### generate docker image tags ################################################
+
+# in master for release
+# if [[ "${CI_COMMIT_BRANCH}" == "master" ]]; then
+#   REGEX='^(schedule|merge_request_event)$'
+#   if [[ "${CI_PIPELINE_SOURCE}" =~ $REGEX ]]; then
+#     echo "export IMAGE_PRIMARY_TAG='latest'"
+#     echo "export IMAGE_SECONDARY_TAGS=\"build-$(date +%Y%m%d) stable\""
+#   fi
+#
+#   if [[ "${CI_PIPELINE_SOURCE}" == "external_pull_request_event" ]]; then
+#     echo "export IMAGE_PRIMARY_TAG='PR-'"
+#     echo "export IMAGE_SECONDARY_TAGS=\"build-$(date +%Y%m%d) stable\""
+#   fi
+# # in any working branch
+# else
+#   echo "export IMAGE_PRIMARY_TAG=$(CI_COMMIT_SHA)"
+#   echo "export IMAGE_SECONDARY_TAGS=$(CI_COMMIT_BRANCH)"
+# fi
+
+
+# # on PR
+# DOCKER_ENV_CI_PIPELINE_SOURCE=external_pull_request_event
+# CI_EXTERNAL_PULL_REQUEST_IID=9
+# CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME=master
+# CI_COMMIT_BRANCH=enable_gitlab_ci
+#
+# # on normal commit in github to working brnach
+# CI_PIPELINE_SOURCE=push
+# CI_COMMIT_BRANCH=enable_gitlab_ci
+# CI_COMMIT_SHORT_SHA=4b89e8a4
 
 
 
