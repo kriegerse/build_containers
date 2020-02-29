cffafa27ab908b5ae6b63be9c9d618444ce42cd4
kriegerse <s_krieger@web.de>
2020-02-29 21:29:05 +0100

    create build_info generation stage

---
+++ generate_build_info.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+export TEST1="TEST1"
+
+
+
+# if [[ "${DRONE_BRANCH}" == "master" && ${DRONE_PULL_REQUEST} == "" ]]; then
+#   echo -n "latest" > .tags
+#   echo -n ",stable" >> .tags
+#   echo -n ",builddate-$(date +%Y-%m-%d)" >> .tags
+# elif [[ ${DRONE_PULL_REQUEST} != "" ]]; then
+#   echo -n "latest" > .tags
+#   echo -n ",testing" >> .tags
+#   echo -n ",pr-${DRONE_PULL_REQUEST}-build-${DRONE_BUILD_NUMBER}" >> .tags
+# else
+#   echo -n "latest" > .tags
+#   echo -n ",dev" >> .tags
+#   echo -n ",branch-${DRONE_BRANCH}-build-${DRONE_BUILD_NUMBER}"
+# fi
