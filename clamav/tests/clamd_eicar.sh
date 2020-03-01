0a10018f34aee7c3dd0996768cdb0e47420096fb
kriegerse <s_krieger@web.de>
2020-03-01 23:11:13 +0100

    enhance test output

--- clamav/tests/clamd_eicar.sh
+++ clamav/tests/clamd_eicar.sh
@@ -13,11 +13,13 @@ echo "* Docker: get clamd port mapping"
 export clamd_map_port=$(docker port clamav-stage_$BUILD_PRIMARY_TAG 3310/tcp | cut -d ':' -f 2)
 echo "port is ${clamd_map_port}"
 
+
 echo "* Docker: waiting for clamd port ready"
 count=0
 while [ $count -lt 60 ]; do
-  echo "PING" | nc 127.0.0.1 ${clamd_map_port}
   count=$(( $count + 1 ))
+  docker exec -it clamav-stage_$BUILD_PRIMARY_TAG  supervisorctl status 
+  echo "PING" | nc 127.0.0.1 ${clamd_map_port}
   sleep 10
 done
 
