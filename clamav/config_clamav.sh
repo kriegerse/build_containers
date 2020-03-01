d6df1d8a98f81f6ab401c1ef08e172ef3b7b2f94
kriegerse <s_krieger@web.de>
2020-03-01 15:28:18 +0100

    reduce RUN layers in dockerfile, let config_clamav.sh do the work

---
+++ clamav/config_clamav.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# get OS release info
+source /etc/os-release
+
+# add security repository and import keys
+zypper -vvv -n ar http://download.opensuse.org/repositories/security/openSUSE_Leap_${VERSION_ID}/security.repo
+zypper -vvv -n --gpg-auto-import-keys refresh
+
+# install clamav and supervisord
+zypper -vvv -n in clamav supervisor
+zypper -vvv -n clean --all
+
+# configure clamav
+chmod +x /usr/local/bin/start_clam.sh
+install -D -d -o vscan -g vscan -m 750 /data/db
+install -D -d -o vscan -g vscan -m 750 /data/conf
+sed -i -e 's|^#DatabaseDirectory|DatabaseDirectory|' /etc/clamd.conf /etc/freshclam.conf
+sed -i -e 's|^DatabaseDirectory.*$|DatabaseDirectory /data/db|' /etc/clamd.conf /etc/freshclam.conf
--- clamav/config_repo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# get OS release info
-source /etc/os-release
-
-# add security repository and import keys
-zypper -vvv --non-interactive ar http://download.opensuse.org/repositories/security/openSUSE_Leap_${VERSION_ID}/security.repo
-zypper -vvv --gpg-auto-import-keys --non-interactive refresh
