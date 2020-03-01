cbd7221efd71c6e94e780f00be3af7830c102eee
kriegerse <s_krieger@web.de>
2020-03-01 22:44:13 +0100

    loop and wait for clamd port

--- clamav/tests/clamd_eicar.sh
+++ clamav/tests/clamd_eicar.sh
@@ -2,6 +2,31 @@
 
 set -x
 
-echo "clamd eicar testing"
+echo "=== clamd EICAR testing ==="
 
-echo "waiting for clamd port"
+echo "* Docker: pull image $docker_username/clamav-stage:$BUILD_PRIMARY_TAG"
+docker pull $docker_username/clamav-stage:$BUILD_PRIMARY_TAG
+
+echo "* Docker start clamav-stage_$BUILD_PRIMARY_TAG"
+docker run -d -P --name clamav-stage_$BUILD_PRIMARY_TAG $docker_username/clamav-stage:$BUILD_PRIMARY_TAG
+
+echo "* Docker: get clamd port mapping"
+export clamd_map_port=$(docker port clamav-stage_$BUILD_PRIMARY_TAG 3310/tcp | cut -d ':' -f 2)
+echo "port is ${clamd_map_port}"
+
+echo "* Docker: waiting for clamd port ready"
+count=0
+while [ $count -lt 60 ]; do
+  echo "PING" | nc 127.0.0.1 ${clamd_map_port}
+  count=$(( $count + 1 ))
+  x=$(( $x + 1 ))
+done
+
+
+# - docker ps -a
+# - docker port clamav-stage_$BUILD_PRIMARY_TAG 3310/tcp
+# - netstat -tulpn
+# - which nc
+# - ip a
+# - export docker_port=$(docker port clamav-stage_$BUILD_PRIMARY_TAG
+#   3310/tcp | cut -d ':' -f 2)
