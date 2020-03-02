2ca92c9eb6f2f5a327c3ffda388dea08b04557d5
kriegerse <s_krieger@web.de>
2020-03-02 20:35:41 +0100

    ping clam port as service

--- clamav/tests/clamd_eicar.sh
+++ clamav/tests/clamd_eicar.sh
@@ -3,39 +3,25 @@
 
 echo "=== clamd EICAR testing ==="
 
-echo "* Docker: pull image $docker_username/clamav-stage:$BUILD_PRIMARY_TAG"
-#docker pull $docker_username/clamav-stage:$BUILD_PRIMARY_TAG
-
-echo "* Docker start clamav-stage_$BUILD_PRIMARY_TAG"
-#docker run --network=bridge -d -P --name clamav-stage_$BUILD_PRIMARY_TAG $docker_username/clamav-stage:$BUILD_PRIMARY_TAG
-
-echo "* Docker: get clamd port mapping"
-export clamd_map_port=$(docker port clamav-stage_$BUILD_PRIMARY_TAG 3310/tcp | cut -d ':' -f 2)
-echo "port is ${clamd_map_port}"
+# echo "* Docker: pull image $docker_username/clamav-stage:$BUILD_PRIMARY_TAG"
+# #docker pull $docker_username/clamav-stage:$BUILD_PRIMARY_TAG
+#
+# echo "* Docker start clamav-stage_$BUILD_PRIMARY_TAG"
+# #docker run --network=bridge -d -P --name clamav-stage_$BUILD_PRIMARY_TAG $docker_username/clamav-stage:$BUILD_PRIMARY_TAG
+#
+# echo "* Docker: get clamd port mapping"
+# export clamd_map_port=$(docker port clamav-stage_$BUILD_PRIMARY_TAG 3310/tcp | cut -d ':' -f 2)
+# echo "port is ${clamd_map_port}"
 
 
 echo "* Docker: waiting for clamd port ready"
-cat /etc/hosts
-ip a
-hostname
-docker network ls
-docker ps -a
-env
-
-echo "loop starts here"
-ping -c1 clamav
-
 
 count=0
 while [ $count -lt 6 ]; do
   count=$(( $count + 1 ))
   docker exec -i clamav-stage_$BUILD_PRIMARY_TAG  supervisorctl status
-  echo "PING localhost"
-  echo "PING" | nc localhost ${clamd_map_port}
-  echo "PING $HOSTNAME"
-  echo "PING" | nc $HOSTNAME ${clamd_map_port}
-  echo "PING dind-service"
-  echo "PING" | nc dind-service ${clamd_map_port}
+  echo "** PING clamav port ${CLAMAV_PORT_3310_TCP_ADDR}:${CLAMAV_PORT_3310_TCP_PORT}"
+  echo 'PING' | nc ${CLAMAV_PORT_3310_TCP_ADDR} ${CLAMAV_PORT_3310_TCP_PORT}
   sleep 10
 done
 
