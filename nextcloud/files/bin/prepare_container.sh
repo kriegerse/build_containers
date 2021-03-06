36ec083b564aada397d03e43061f8636e67d37fc
kriegerse <s_krieger@web.de>
2021-03-06 20:41:34 +0100

    build nextcloud with own requirements

---
+++ nextcloud/files/bin/prepare_container.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# exit on any error
+set -ex
+
+# DEFINE VARS
+OS_PACKAGES="less ffmpeg libmagickcore-6.q16-6-extra libsmbclient"
+OS_PACKAGES_TEMP="libbz2-dev libc-client-dev libkrb5-dev libsmbclient-dev"
+NC_APPS="apporder bookmarks calendar contacts deck files_accesscontrol
+files_antivirus files_automatedtagging files_downloadactivity files_markdown
+files_mindmap keeweb metadata news notes previewgenerator
+tasks twofactor_totp files_retention"
+
+### install additional libraries + tools ###
+apt-get update
+apt-get install -y --no-install-recommends \
+   ${OS_PACKAGES}
+
+### install additional php modules ###
+apt-get install -y --no-install-recommends \
+   ${OS_PACKAGES_TEMP}
+
+# php-imap
+docker-php-ext-configure imap --with-kerberos --with-imap-ssl
+docker-php-ext-install imap
+docker-php-ext-enable imap
+# php-bz2
+docker-php-ext-install bz2
+docker-php-ext-enable bz2
+# smbclient (use pecl)
+pecl install smbclient
+docker-php-ext-enable smbclient
+
+
+apt-get purge -y \
+   ${OS_PACKAGES_TEMP}
+
+
+### install nextcloud apps ###
+# make apps not stored in apps_custom
+cd /usr/src/nextcloud/config
+mv apps.config.php apps.config.php_orig
+# create temporary nextcloud installation
+cd /usr/src/nextcloud
+php occ maintenance:install \
+   --admin-pass=verysecure \
+   --data-dir=/tmp/nc_data
+# install NC apps
+for APP in ${NC_APPS} ; do
+  php occ app:install --keep-disabled -vvv ${APP}
+done
+
+
+# notify_push requires special treating (https://github.com/nextcloud/notify_push/issues/54)
+php occ app:install --keep-disabled -vvv notify_push || true
+
+# maps unavailable due to
+# https://github.com/nextcloud/maps/issues/541
+# root@7413d5fbe17a:/usr/src/nextcloud# php occ app:install -vvv  --keep-disabled maps
+# Error: The "unique" column option is not supported.
+php occ app:install --keep-disabled -vvv maps || true
+
+# restore saved config and cleanup
+cd /usr/src/nextcloud/config
+mv apps.config.php_orig apps.config.php
+rm config.php
+rm -rf /tmp/nc_data /usr/src/nextcloud/data/*
+
+### self cleanup ###
+apt-get clean -y
+rm -rf /var/lib/apt/lists/*
+rm -f $0
